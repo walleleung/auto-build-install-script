625d68be8b1e3f14854b2e9f6af1016970c038a7
wenin819 <wenin819@gmail.com>
2013-09-06 16:01:30 +0800

    增加主shell和常用的shell函数

---
+++ lib/lc_shell_lib.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# 检查目录是否存在，不存在自动创建
+function ckdir()
+{
+	fun_info="检查目录是否存在："
+	if [ -n "$1" ] ; then
+		echo $fun_info失败，目录没有指定
+		return 1
+	elif ! [ -d "$1" ] ; then
+		echo 目录$1不存在，自动创建
+		mkdir -p $1
+	fi  
+}
+
+# 通过程序名称关键字进行结束程序
+function killprogs()
+{
+	fun_info="通过程序关键字结束程序："
+	if [ -n "$1" ] ; then
+		prog_name="$1"
+	else
+		echo $fun_info失败，程序关键字没有指定
+		return 1
+	fi
+	prog_count=`ps -ef | grep "$prog_name" | grep -v "grep" | wc -l`
+	if [ $prog_count -eq 0 ] ; then
+		echo $fun_info程序$prog_name未运行，不做处理
+		return 0
+	fi
+	keys=`ps -ef |grep "$prog_name" | grep -v "grep" | awk '{print $2}'`
+	for key in ${keys}
+do
+	kill -9 $key
+done
+}
+
+# 替换zip中的文件
+function replace_zip()
+{
+	if [ $# -ge 2 ] ; then
+		zip -d $*
+		zip -u $*
+	fi
+}
+
+# 判断命令是否存在
+function check_cmd_exists()
+{
+	fun_name="$1"
+	if [ -n "$fun_name" ] ; then
+		[ `type "$fun_name" >/dev/null 2>&1 | head -1 | grep "/" | wc -l` -eq 0 ] && return 1
+		return 0
+	else
+		return 1
+	fi
+}
+
+# 判断函数是否存在
+function check_is_fun()
+{
+	fun_name="$1"
+	if [ -n "$fun_name" ] ; then
+		type "$fun_name" >/dev/null 2>&1 || return 1
+		[ `type "$fun_name" >/dev/null 2>&1 | head -1 | grep "/" | wc -l` -gt 0 ] && return 1
+		return 0
+	else
+		return 1
+	fi
+}
+
+# 得到最终的程序文件
+function get_final_cmd()
+{
+	fun_info="得到命令最终的程序文件："
+	PRG="$1"
+	if [ -z "$PRG" ] ; then
+		#echo $fun_info失败，没有给定命令名
+		return 1
+	elif ! [ -f "$PRG" ] ; then
+		PRG=`command -v "$PRG"`
+	fi
+
+	while [ -h "$PRG" ]; do
+		ls=`ls -ld "$PRG"`
+		link=`expr "$ls" : '.*-> \(.*\)$'`
+		if expr "$link" : '/.*' > /dev/null; then
+			PRG="$link"
+		else
+			PRG=`dirname "$PRG"`/"$link"
+		fi  
+	done
+	echo $PRG
+}
